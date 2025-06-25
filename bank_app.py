cca169c89841265b510ed554903c6d2e226a281c
gelaiii10 <gelaibacleaan@gmail.com>
2025-06-24 19:15:18 -0800

    added the class

--- bank_app.py
+++ bank_app.py
@@ -4,3 +4,9 @@ from tkinter import messagebox
 #hard coded credentials
 USERNAME = "admin123"
 PASSWORD = "12345"
+
+#bank app class
+class BankApp:
+    def __init__(self, root):
+        self.balance = 0
+        self.history = []
