a396cacfa9356c974d7fcdbda6768c58aa25e76c
gelaiii10 <gelaibacleaan@gmail.com>
2025-06-24 19:43:30 -0800

    init function finished

--- bank_app.py
+++ bank_app.py
@@ -10,3 +10,24 @@ class BankApp:
     def __init__(self, root):
         self.balance = 0
         self.history = []
+
+        root.title("Mini Bank Account")
+        root.geometry("350x300")
+
+        self.balance_label = tk.Label(root, text=f"Balance: ₱{self.balance}", font = ("Avenir", 14))
+        self.balance_label.pack(pady = 10)
+
+        self.amount_entry = tk.Entry(root, width = 20)
+        self.amount_entry.pack(pady = 5)
+
+        self.deposit_button = tk.Button(root, text="Deposit", command = self.deposit)
+        self.deposit_button.pack(pady = 5)
+
+        self.withdraw_button = tk.Button(root, text="Withdraw", command = self.withdraw)
+        self.withdraw_button.pack(pady = 5)
+
+        self.history_label = tk.Label(root, text= "Transaction History", font = ("Avenir", 10, "bold"))
+        self.history_label.pack(pady = 10)
+
+        self.history_box = tk.Text(root, height = 6, width = 40)
+        self.history_box.pack()
